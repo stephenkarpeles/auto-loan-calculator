042fe544bc9c0ec3b1b3b380a3a4e79cb650f01d
Stephen Karpeles <stephen@stephenkarpeles.com>
2019-09-15 23:00:50 -0500

    styles

--- src/components/AutoLoanCalculator.vue
+++ src/components/AutoLoanCalculator.vue
@@ -1,16 +1,38 @@
 <template>
   <div class="auto-loan-caculator">
-    <p>Car Price</p>
-    <input class="field--price" type="number" name="carPrice" v-model.number="carPrice" placeholder="">
+    <div class="form-field-row">
+      <label for="carPrice" class="label label--price">Car Price</label><br>
+      <input 
+        class="form-field form-field--price" 
+        type="number" 
+        name="carPrice" 
+        v-model.number="carPrice" 
+        placeholder="">
+    </div>
 
-    <p>Down Payment</p>
-    <input class="field--down-payment" type="number" name="downPayment" v-model.number="downPayment" placeholder="">
+    <label for="downPayment" class="label label--price">Down Payment</label><br>
+    <input 
+      class="form-field form-field--down-payment" 
+      type="number" 
+      name="downPayment" 
+      v-model.number="downPayment" 
+      placeholder="">
 
     <p>Loan Duration (in months)</p>
-    <input class="field--duration" type="number" name="loanDurationMonths" v-model.number="loanDurationMonths" placeholder="">
+    <input 
+      class="form-field form-field--duration" 
+      type="number" 
+      name="loanDurationMonths"
+      v-model.number="loanDurationMonths" 
+      placeholder="">
 
     <p>Interest Rate</p>    
-    <input class="field--interest" type="number" name="interestRate" v-model.number="interestRate" placeholder="">
+    <input 
+      class="form-field form-field--interest" 
+      type="number" 
+      name="interestRate" 
+      v-model.number="interestRate" 
+      placeholder="">
 
     <hr>
 
@@ -27,10 +49,10 @@
     name: 'auto-loan-caculator',
     data() {
       return {
-        carPrice: '20000',
-        downPayment: '10000',
-        loanDurationMonths: '48',
-        interestRate: '5',
+        carPrice: '',
+        downPayment: '',
+        loanDurationMonths: '',
+        interestRate: '',
         monthlyPayment: '',
         totalInterestPaid: '',
       }
@@ -40,20 +62,20 @@
       totalInterestPaid: Number
     },
     computed: {
+      totalPrincipal: function() {
+        return this.carPrice - this.downPayment || 0;
+      },
       monthlyPayment: function() {
-        const principal = this.carPrice - this.downPayment;
         const monthlyInterestRate = this.interestRate / 100 / 12;
         const numberMonths = this.loanDurationMonths;
 
         // The meat and potatos calculation
-        const estMonthlyAmount = principal * (monthlyInterestRate * (1 + monthlyInterestRate)**numberMonths) / ((1+monthlyInterestRate)**numberMonths-1) || 0;
+        const estMonthlyAmount = this.totalPrincipal * (monthlyInterestRate * (1 + monthlyInterestRate)**numberMonths) / ((1+monthlyInterestRate)**numberMonths-1) || 0;
 
         return Math.round(estMonthlyAmount);
       },
       totalInterestPaid: function() {
-        const totalPrincipal = this.carPrice - this.downPayment;
-
-        return this.loanDurationMonths * this.monthlyPayment - totalPrincipal;
+        return this.loanDurationMonths * this.monthlyPayment - this.totalPrincipal || 0;
       },
     }, 
   }
@@ -66,5 +88,23 @@
 
   input {
     margin-bottom: 1rem;
+    padding: .5rem;
+    align-self: center;
+  }
+
+  .form-field-row {
+    display: flex;
+  }
+
+  .label {
+    position: relative;
+    width: 10rem;
+    align-self: center;
+  }
+
+  .label:after {
+    content: '$';
+    left: 10.5rem;
+    position: absolute;
   }
 </style>
