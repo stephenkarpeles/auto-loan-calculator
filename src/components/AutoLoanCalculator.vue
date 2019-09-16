441bb6465c12e5640fd37321ffae51003622f42a
Stephen Karpeles <stephen@stephenkarpeles.com>
2019-09-15 21:59:08 -0500

    Fixed calculations

--- src/components/AutoLoanCalculator.vue
+++ src/components/AutoLoanCalculator.vue
@@ -1,56 +1,61 @@
 <template>
   <div class="auto-loan-caculator">
     <p>Car Price</p>
-    <input class="field--price" type="number" name="carPrice" v-model="carPrice" placeholder="$20,000">
+    <input class="field--price" type="number" name="carPrice" v-model.number="carPrice" placeholder="">
 
     <p>Down Payment</p>
-    <input class="field--down-payment" type="number" name="downPayment" v-model="downPayment" placeholder="$20,000">
+    <input class="field--down-payment" type="number" name="downPayment" v-model.number="downPayment" placeholder="">
 
     <p>Loan Duration (in months)</p>
-    <input class="field--duration" type="number" name="loanDurationMonths" v-model="loanDurationMonths" placeholder="48">
+    <input class="field--duration" type="number" name="loanDurationMonths" v-model.number="loanDurationMonths" placeholder="">
 
     <p>Interest Rate</p>    
-    <input class="field--interest" type="number" name="interestRate" v-model="interestRate" placeholder="8">
+    <input class="field--interest" type="number" name="interestRate" v-model.number="interestRate" placeholder="">
 
     <hr>
 
-    <p>Monthly Payment</p>
-    <input class="field--monthly-payment" type="text" id="monthlyPayment" v-model="monthlyPayment">
+    <p>Estimated Monthly Payment</p>
+    <div>{{ monthlyPayment }}</div>
 
     <p>Total Interest Paid (over duration of loan)</p>
-    <input class="field--interest-paid" type="text" id="totalInterestAmount" v-model="totalInterestAmount">
+    <div>{{ totalInterestPaid }}</div>
   </div>
 </template>
 
 <script>
   export default {
     name: 'auto-loan-caculator',
-    data () {
+    data() {
       return {
         carPrice: '20000',
         downPayment: '10000',
         loanDurationMonths: '48',
-        interestRate: '8',
+        interestRate: '5',
+        monthlyPayment: '',
+        totalInterestPaid: '',
       }
+    },
+    props: {
+      monthlyPayment: Number,
+      totalInterestPaid: Number
     },
     computed: {
       monthlyPayment: function() {
-        return ( parseInt(this.carPrice) - parseInt(this.downPayment) ) * ((parseInt(this.interestRate)/12) * ((1 + (parseInt(this.interestRate)/12))**parseInt(this.loanDurationMonths))) / ( (1 + (parseInt(this.interestRate)/12))**parseInt(this.loanDurationMonths) - 1);
-        // return parseInt(this.carPrice) + parseInt(this.downPayment);
+        const principal = this.carPrice - this.downPayment;
+        const monthlyInterestRate = this.interestRate / 100 / 12;
+        const numberMonths = this.loanDurationMonths;
+
+        // The meat and potatos calculation
+        const estMonthlyAmount = principal * (monthlyInterestRate * (1 + monthlyInterestRate)**numberMonths) / ((1+monthlyInterestRate)**numberMonths-1) || 0;
+
+        return Math.round(estMonthlyAmount);
       },
-      totalInterestAmount: function() {
-        return ;
+      totalInterestPaid: function() {
+
       },
     }, 
   }
 </script>
-
-A= P*(r(1+r)^{n}) / ((1+r)^{n}-1)
-
-A = the monthly payment.
-P = the principal
-r = the interest rate per month, which equals the annual interest rate divided by 12
-n = the total number of months
 
 <style scoped>
   p {
